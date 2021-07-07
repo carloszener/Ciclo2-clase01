01e6f79321fbe00d283fd3bc6f046c8f23f1dd2c
carloszener <carloszener81@gmail.com>
2021-07-06 21:13:39 -0500

    Segunda Version

--- EjerU1_05.java
+++ EjerU1_05.java
@@ -6,28 +6,26 @@ public class EjerU1_05 {
     public static void main(String[] args) {
         var sc = new Scanner(System.in);
 
-        System.out.println("Introduzca un numero de 0 a 99: \f");
-        var numero1 = sc.nextInt();
-        sc.close();
+        Random r = new Random();
+        var numero1 = r.nextInt(99)+1;
+        var numero2 = 0;
+
+        while (numero1 != numero2) {
         
-        Random rnd = new Random();
-        var numero2 = (int)(rnd.nextDouble() * 100 + 0);
-        System.out.println(numero2);
+          System.out.println("Introduzca un numero de 1 a 100: \f");
+          numero2 = sc.nextInt();
+          
+          if (numero2 > numero1) {
+            System.out.println("Su numero es mayor ");
+          }
+          if (numero2 < numero1) {
+            System.out.println("Su numero es menor ");
+          }
+          if (numero2 == numero1) {
+            System.out.println("Adivinaste el numero");
+          }  
 
-        if (numero1 <= 99) {
-
-              if (numero2 == numero1) {
-                System.out.println("Adivinaste el numero");
-              }  
-              if (numero2 > numero1) {
-                System.out.println("El numero es mayor ");
-              }
-              if (numero2 < numero1) {
-                System.out.println("El numero es menor ");
-              }
         }
-        else {
-            System.out.println("Numero no valido");
-        }
+        sc.close();
     }
 }
